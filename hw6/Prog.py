5aa0432864f1894ae797de3357d6e5e839b4f655
Giang Duong <69888109+giangduong96@users.noreply.github.com>
2021-04-18 14:07:02 -0700

    Add files via upload

---
+++ hw6/Prog.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 14:54:17 2020
+
+@author: sanjo
+"""
+
+import argparse
+parser = argparse.ArgumentParser()
+#parser.add_argument("echo")
+args = parser.parse_args()
+#print(args.echo)
